850e380b8fbc680e6d948e920a57fdf6ea058c43
emvalencaf <edsonmvf@gmail.com>
2024-09-16 18:07:05 -0300

    :heavy_plus_sign: build: Change database for PostgreSQL
    Added PostgreSQL for the databse and removed MongoDB
    Added scripts for run a docker container for run the postgres database

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -3,16 +3,17 @@ import * as dotenv from 'dotenv';
 import { DataSource, DataSourceOptions } from 'typeorm';
 
 dotenv.config({
-    path: '.development.env',
+    path: '.env.development.local',
 });
 
 const config: TypeOrmModuleOptions & DataSourceOptions = {
-    type: 'mongodb',
+    type: 'postgres',
     host: process.env.DB_HOST,
     port: Number(process.env.DB_PORT) || 5432,
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
+    schema: process.env.DB_SCHEMA,
     entities: [`${__dirname}/**/*.entity{.js,.ts}`],
     migrations: [`${__dirname}/migrations/**/*{.ts,.js}`],
     synchronize: false, // Deve ser false em produção
@@ -22,5 +23,4 @@ const config: TypeOrmModuleOptions & DataSourceOptions = {
 export default config;
 
 // create the datasource for CLI TypeORM
-
 export const AppDataSource = new DataSource(config);
